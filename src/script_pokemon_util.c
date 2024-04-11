dc8812117810ada8e0ba31652da21726146c9f1f
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2024-04-11 21:34:11 +0200

    Fixes givemon moves not defaulting to none (#4377)

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -358,7 +358,9 @@ u32 ScriptGiveMonParameterized(u16 species, u8 level, u16 item, u8 ball, u8 natu
     // moves
     for (i = 0; i < MAX_MON_MOVES; i++)
     {
-        if (moves[i] == MOVE_NONE || moves[i] >= MOVES_COUNT)
+        if (moves[0] == MOVE_NONE)
+            break;
+        if (moves[i] >= MOVES_COUNT)
             continue;
         SetMonMoveSlot(&mon, moves[i], i);
     }
@@ -553,7 +555,7 @@ u32 BirchCase_GiveMonParameterized(u16 species, u8 level, u16 item, u8 ball, u8 
     }
 
     // set pokédex flags
-    nationalDexNum = SpeciesToNationalPokedexNum(species); 
+    nationalDexNum = SpeciesToNationalPokedexNum(species);
     switch (sentToPc)
     {
     case MON_GIVEN_TO_PARTY:
@@ -667,7 +669,7 @@ void Script_SetStatus1(struct ScriptContext *ctx)
         }
     }
     else
-    {        
+    {
         SetMonData(&gPlayerParty[slot], MON_DATA_STATUS, &status1);
     }
 }
