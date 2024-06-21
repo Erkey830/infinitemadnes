eda3096cb0ef3aa23796aabb03ce32d38dc8762f
erkey830 <erkey.official@gmail.com>
2024-06-21 18:58:56 +0200

    fixes + better mapping hehe

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -98,7 +98,6 @@
 #define MAP_ROUTE48             (89 | (0 << 8))
 #define MAP_CHAMPION_ROUTE      (90 | (0 << 8))
 #define MAP_NATIONAL_PARK       (91 | (0 << 8))
-#define MAP_GROMENT_CITY        (92 | (0 << 8))
 
 // gMapGroup_IndoorLittleroot
 #define MAP_LITTLEROOT_TOWN_BRENDANS_HOUSE_1F    (0 | (1 << 8))
